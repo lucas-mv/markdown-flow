5daf55c818606301c3d5234f9e7de22411adf1a5
Lucas Moura Veloso <lucas.veloso@mrv.com.br>
2018-10-01 14:08:50 -0300

    mudança de css para scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import * as shape from 'd3-shape';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
